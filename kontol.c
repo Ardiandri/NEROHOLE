595e486d71695be17948494c8e2d0813dd3dad53
Ardiandri <im.a.droid88@gmail.com>
2026-01-28 02:06:40 +0700

    Add files via upload

---
+++ kontol.c
@@ -0,0 +1,15 @@
+A64HookFunction((void *) (anogs + oxorany(0x5204D0)), (void *)ANOSDK, nullptr); 
+A64HookFunction((void *) (anogs + oxorany(0x2328F0)), (void *)ANOSDK, nullptr); 
+A64HookFunction((void *) (anogs + oxorany(0x228168)), (void *)ANOSDK, nullptr); 
+//A64HookFunction((void *) (anogs + oxorany(0x5201E0)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x3A560C)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x3A564C)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x229134)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x37C904)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x387A3C)), (void *)ANOSDK, nullptr);
+
+A64HookFunction((void *) (anogs + oxorany(0x436B18)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x43B62C)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x446AD8)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x447750)), (void *)ANOSDK, nullptr);
+A64HookFunction((void *) (anogs + oxorany(0x44B9D0)), (void *)ANOSDK, nullptr); //Fix Crash __strncpy
